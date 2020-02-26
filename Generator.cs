5c8e85c329ede47c596da45fce22cc62cad194e2
Mike Nichols <mike.nichols97@gmail.com>
2020-02-25 22:13:58 -0500

    Add files via upload

---
+++ Generator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+
+namespace RandomGenerator
+{
+    public class Generator
+    {
+        public int randomInt(int seed)
+        {
+            var random = new Random(seed);
+            return random.Next();
+        }
+
+        public decimal randomDecimal(int seed)
+        {
+            var random = new Random(seed);
+            return Convert.ToDecimal(random.NextDouble());
+        }
+
+        public int randomRange(int seed, int min, int max)
+        {
+            var random = new Random(seed);
+            return random.Next(min, max);
+        }
+
+        public decimal randomRange(int seed, decimal min, decimal max)
+        {
+            var random = new Random(seed);
+            return Convert.ToDecimal(random.NextDouble()) * (max-min) + min;
+        }
+
+        public int[] randomIntList(int seed, int length)
+        {
+            int[] list = new int[length];
+            for(int i = 0; i < length; i++)
+            {
+                var random = new Random(seed + i);
+                list[i] = random.Next();
+            }
+            return list;
+        }
+
+        public decimal[] randomDecimalList(int seed, int length)
+        {
+            decimal[] list = new decimal[length];
+            for (int i = 0; i < length; i++)
+            {
+                var random = new Random(seed + i);
+                list[i] = Convert.ToDecimal(random.NextDouble());
+            }
+            return list;
+        }
+
+
+    }
+}
